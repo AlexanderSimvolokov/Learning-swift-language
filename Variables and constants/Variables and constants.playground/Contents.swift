e225674a264bc9842733b21f8eff25720ba5e4f2
Alexander Simvolokov <alexandersimvolokov@Alexanders-MacBook-Air.local>
2023-02-04 10:58:00 +0300

    Learning variable

--- Variables and constants/Variables and constants.playground/Contents.swift
+++ Variables and constants/Variables and constants.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+var valueVarInt: Int
+var valueVarString: String
+var valueVarFloat: Float
+var valueVarDouble: Double
 
-var greeting = "Hello, playground"
+let valueLetInt: Int
+let valueLetString: String
+let valueLetFloat: Float
+let valueLetDouble: Double
+
+var typeApple = "Macintosh"
+var countApple = 5
+print("I have \(countApple) apple")
+var textAboutApples1 = "I have " + String(countApple) + " apple"
+var textAboutApples2 = "I have \(countApple) apple"
+
+let multilineText = """
+This
+my
+code!
+"""
